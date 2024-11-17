578624f6bed73a7928889d35b03a128383403ad1
Domajn <141944218+Domajn@users.noreply.github.com>
2024-11-17 11:57:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+L = [];
+        L[0] = ['00', '01.130', '11 Feb 2024', true, true];
+        L[1] = ['01', '00.560', 'Unknown', false, true];
+        L[2] = ['02', '00.480', 'Unknown', false, false];
+        L[3] = ['03', '02.080', '11 Sep 2024', true, false];
+        L[4] = ['04', '00.960', '24 Oct 2024', true, true];
+        L[5] = ['05', '04.100', '24 Oct 2024', true, true];
+        L[6] = ['06', '04.600', 'Unknown', false, true];
+        L[7] = ['07', '02.260', '24 Oct 2024', true, true];
+        L[8] = ['08', '02.680', 'Unknown', false, true];
+        L[9] = ['09', '02.220', 'Unknown', false, false];
+        L[10] = ['10', '04.260', 'Unknown', false, false];
+        L[11] = ['11', '04.310', '19 Feb 2024', true, true];
+        L[12] = ['12', '02.500', '12 Feb 2024', true, true];
+        L[13] = ['13', '01.130', 'Unknown', false, true];
+        L[14] = ['14', '05.980', '24 Oct 2024', true, true];
+        L[15] = ['15', '02.460', 'Unknown', false, true];
+        L[16] = ['16', '10.810', '23 Oct 2024', true, true];
+        L[17] = ['17', '03.900', '23 Oct 2024', true, true];
+        L[18] = ['18', '06.460', 'Unknown', false, true];
+        L[19] = ['19', '07.050', 'Unknown', false, true];
+        L[20] = ['20', '01.180', '24 Oct 2024', true, true];
+        L[21] = ['21', '08.390', '22 Sep 2024', true, true];
+        L[22] = ['22', '03.450', '24 Oct 2024', true, true];
+        L[23] = ['23', '03.800', '22 Sep 2024', true, true];
+        L[24] = ['24', '04.410', '24 Oct 2024', true, true];
+        L[25] = ['25', '04.980', '24 Oct 2024', true, true];
+        L[26] = ['26', '06.070', 'Unknown', false, true];
+        L[27] = ['27', '20.640', '16 Feb 2024', true, true];
+        L[28] = ['28', '07.710', '24 Oct 2024', true, true];
+        L[29] = ['29', '06.680', '31 Oct 2024', false, false];
+
+        /*
+
+            SRC DATA
+
+            05 Feb 2024 x1 (18)
+            06 Feb 2024 x1 (01)
+            11 Feb 2024 x2 (00, 13)
+            12 Feb 2024 x3 (12, 15, 26)
+            16 Feb 2024 x2 (02, 27)
+            19 Feb 2024 x2 (11, 19)
+            22 Feb 2024 x1 (06)
+            11 Sep 2024 x1 (03)
+            21 Sep 2024 x3 (08, 21, 23)
+            23 Oct 2024 x1 (17)
+            25 Oct 2024 x10 (04, 05, 07, 14, 16, 20, 22, 24, 25, 28)
+            Unknown x3 (09, 10, 29)
+
+            ACTUAL DATES
+
+            Level 16 PBed on the 23rd of October
+            All October 25th PBs were done on the 24th
+
+        */
